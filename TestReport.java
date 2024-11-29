6898c5623ea52726dd10ca3923072dbbe27a5691
paramgitworks <161829485+paramgitworks@users.noreply.github.com>
2024-11-29 14:19:25 +0530

    Update TestReport.java

--- TestReport.java
+++ TestReport.java
@@ -1294,5 +1294,68 @@ public class Tek_Properties {
 			t.printStackTrace();
 		}		
 	}
+
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.apache.wss4j.dom.WSSecSignature;
+import org.apache.wss4j.dom.message.WSSecHeader;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPMessage;
+import java.io.FileInputStream;
+import java.io.StringWriter;
+import java.security.KeyStore;
+
+public class WSRequest {
+    public static void main(String[] args) throws Exception {
+        // Path to JKS and Password
+        String jksPath = "/path/to/your.jks";
+        String keystorePassword = "yourKeystorePassword";
+
+        // Load the KeyStore
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        keyStore.load(new FileInputStream(jksPath), keystorePassword.toCharArray());
+
+        // Get the default alias
+        String keyAlias = keyStore.aliases().nextElement();
+
+        // Create a SOAP Message
+        MessageFactory messageFactory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = messageFactory.createMessage();
+        SOAPBody body = soapMessage.getSOAPBody();
+        body.addChildElement("exampleElement").setValue("exampleValue");
+
+        // Convert SOAPMessage to Document
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document soapDocument = builder.parse(soapMessage.getSOAPPart().getContent());
+
+        // Add WS-Security Header
+        WSSecHeader secHeader = new WSSecHeader();
+        secHeader.insertSecurityHeader(soapDocument);
+
+        // Sign the SOAP Document
+        WSSecSignature signature = new WSSecSignature();
+        signature.setUserInfo(keyAlias, keystorePassword); // Use retrieved alias and password
+        signature.build(soapDocument, keyStore, secHeader);
+
+        // Convert Document back to String for REST Assured
+        StringWriter writer = new StringWriter();
+        javax.xml.transform.Transformer transformer = javax.xml.transform.TransformerFactory.newInstance().newTransformer();
+        transformer.transform(new javax.xml.transform.dom.DOMSource(soapDocument), new javax.xml.transform.stream.StreamResult(writer));
+
+        // Send SOAP Request with Rest Assured
+        RequestSpecification request = RestAssured.given();
+        request.body(writer.toString())
+                .header("Content-Type", "text/xml")
+                .post("https://your.api.endpoint");
+    }
+}
 	
 }
